57ebe4258e0b4f8b531b5b812b04346766440d23
Poonam Shahadev Channe <poonam@Poonams-MacBook-Air.local>
2025-01-17 10:05:12 +0530

    add the env file for secret keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,30 +1,29 @@
-export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+export const LOGO =
+  "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-
-export const AVATAR_URL = "https://occ-0-4957-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXYofKdCJceEP7pdxcEZ9wt80GsxEyXIbnG_QM8znksNz3JexvRbDLr0_AcNKr2SJtT-MLr1eCOA-e7xlDHsx4Jmmsi5HL8.png?r=1d4"
-
+export const AVATAR_URL =
+  "https://occ-0-4957-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXYofKdCJceEP7pdxcEZ9wt80GsxEyXIbnG_QM8znksNz3JexvRbDLr0_AcNKr2SJtT-MLr1eCOA-e7xlDHsx4Jmmsi5HL8.png?r=1d4";
 
 export const BG_IMG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDQ0NmU4YmZlMzMzMmY5ZGJiOWM4MDFiY2RhZjZlMCIsIm5iZiI6MTczNTYyMjk1NS41ODQsInN1YiI6IjY3NzM4MTJiMWMxZDdlMDljZjYxMzg1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dbaTNIyW8w8iYiHHWBZLH9NYVfGOVKiSS-HrscUU4_Y",
+  },
+};
 
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
-  export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDQ0NmU4YmZlMzMzMmY5ZGJiOWM4MDFiY2RhZjZlMCIsIm5iZiI6MTczNTYyMjk1NS41ODQsInN1YiI6IjY3NzM4MTJiMWMxZDdlMDljZjYxMzg1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dbaTNIyW8w8iYiHHWBZLH9NYVfGOVKiSS-HrscUU4_Y'
-    }
-  };
+export const SUPPORTED_LANGAUGE = [
+  { identifier: "en", langName: "English" },
+  { identifier: "hindi", langName: "Hindi" },
+  { identifier: "spanish", langName: "Spanish" },
+  { identifier: "japanese", langName: "Japanese" },
+];
 
+export const OPEN_AI_KEY = process.env.REACT_APP_OPEN_AI_KEY
 
-  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
-
-
-  export const SUPPORTED_LANGAUGE =  [
-    {identifier: "en", langName: "English"},
-    {identifier: "hindi", langName: "Hindi"},
-    {identifier: "spanish", langName: "Spanish"},
-    {identifier: "japanese", langName: "Japanese"},
-  ]
-  
